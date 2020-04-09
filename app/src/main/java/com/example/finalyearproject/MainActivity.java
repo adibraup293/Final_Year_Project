6b1cb7aef62064f6340eaae40b6a53774a5c5c15
adibraup293 <36180086+adibraup293@users.noreply.github.com>
2020-04-10 01:58:17 +0800

    Updated design

--- app/src/main/java/com/example/finalyearproject/MainActivity.java
+++ app/src/main/java/com/example/finalyearproject/MainActivity.java
@@ -2,9 +2,22 @@ package com.example.finalyearproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void openSignUpPage(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToLogin(View view) {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
